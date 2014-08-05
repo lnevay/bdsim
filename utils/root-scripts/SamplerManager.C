e73c7ce6cb57bc6f5a06b25af7f2771ddc607daf
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2014-08-05 20:56:55 +0100

    More work on SamplerManager
    Former-commit-id: 2f12f7889fa5ff72989cb4a4a3dfa4df5715b680

--- utils/root-scripts/SamplerManager.C
+++ utils/root-scripts/SamplerManager.C
@@ -1,23 +1,86 @@
 #include "SamplerManager.h"
 
-#include <iostream>
 
-SamplerManager::SamplerManager(TString fileNameIn) : fileName(fileNameIn) 
+SamplerManager::SamplerManager() 
+{  
+}
+
+SamplerManager::SamplerManager(TString fileName)  
 {
-  std::cout << fileName << std::endl;
-  file = new TFile(fileName);
+  AddFile(fileName);
   Init();
 }
 
 SamplerManager::~SamplerManager() {
-  delete file;
+}
+
+void SamplerManager::AddFile(TString fileName) { 
+  TFile *f = new TFile(fileName); 
+  files.push_back(f);
+  fileNames.push_back(fileName);
 }
 
 void SamplerManager::Init() {
   // loop over available trees in file
-  
+  TList *listOfKeys = files[0]->GetListOfKeys();
+  for(int i=0;i<listOfKeys->GetSize();i++) {
+    TKey *key = (TKey*)listOfKeys->At(i);
+    const char *className = key->GetClassName();
+    const char *objName   = key->GetName(); 
+    const char *objTitle  = key->GetTitle(); 
+#ifdef DEBUG        
+    std::cout << i << " " << className << " \"" << objTitle << "\" " << objName;
+#endif 
+    // check for tree and sampler output
+    if(TString(className) != "TTree" || TString(objTitle) != "Sampler output")  {
+#ifdef DEBUG
+      std::cout << std::endl;      
+#endif 
+      continue; 
+    }
+    else {
+#ifdef DEBUG
+      std::cout << " ADDING TO MANAGER" << std::endl;
+#endif
+    }
+
+    // Add tree to maps
+    treeNames.push_back(objName);
+  }
+
+  // Loop over trees and files to form chains 
+  for(std::vector<TString>::iterator tit = treeNames.begin(); tit != treeNames.end(); tit++) {
+    TChain *c = new TChain(*tit);    
+    for(std::vector<TString>::iterator fit = fileNames.begin(); fit != fileNames.end(); fit++) { 
+      c->Add(*fit);
+    }
+    chains.push_back(c);
+  }
 }
 
 void SamplerManager::PrintTrees() { 
-  
+  for(std::vector<TString>::iterator it = treeNames.begin(); it != treeNames.end(); it++) {
+    std::cout << *it << std::endl;
+  }
+}
+
+void SamplerManager::PrintFiles() { 
+  for(std::vector<TString>::iterator it = fileNames.begin(); it != fileNames.end(); it++) {
+    std::cout << *it << std::endl;
+  }  
+}
+
+void SamplerManager::MakeSingleTree() {
+  for(std::vector<TChain*>::iterator it = chains.begin(); it != chains.end(); it++) { 
+    std::cout << (*it)->GetEntries() << std::endl;
+  }
+
+  for(int i=0;i<(int)chains.size();i++) { 
+    Sampler *s = new Sampler(chains[i]);
+    samplers.push_back(s);
+ }
+    
+  for(int i=0;i<(int)chains[0]->GetEntries();i++) { 
+    
+  }
 }
--- utils/root-scripts/SamplerManager.h
+++ utils/root-scripts/SamplerManager.h
@@ -3,24 +3,36 @@
 
 
 #include <vector> 
+#include <iostream>
 
 #include "TString.h"
 #include "TFile.h"
 #include "TTree.h"
+#include "TList.h"
+#include "TKey.h"
+#include "TChain.h"
+
+#include "Sampler.h"
 
 class SamplerManager {
  public : 
+  SamplerManager();
   SamplerManager(TString fileName);
   ~SamplerManager();
 
+  void AddFile(TString fileName);
   void Init();
   void PrintTrees(); 
-  
+  void PrintFiles();
+  void MakeSingleTree(); 
+
  private:
-  TString fileName;
   TFile  *file;  
-  std::vector<TString> treeNames; 
-  std::vector<TTree*>  trees;
+  std::vector<TString>  fileNames;
+  std::vector<TString>  treeNames; 
+  std::vector<TFile*>   files;
+  std::vector<TChain*>  chains;
+  std::vector<Sampler*> samplers;
 };
 
 
