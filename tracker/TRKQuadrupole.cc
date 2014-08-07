4c628dea80d3d61141d6de9de456eeee1f1a9631
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2014-08-07 15:49:56 +0100

    Ok it compiles now
    Former-commit-id: ecc565c291be1817543ccbb6ef2cb6895f771acf

---
+++ tracker/TRKQuadrupole.cc
@@ -0,0 +1,5 @@
+#include "TRKQuadrupole.hh"
+
+TRKQuadrupole::TRKQuadrupole() {} 
+TRKQuadrupole::TRKQuadrupole(double length, double b) {}
+TRKQuadrupole::~TRKQuadrupole() {}
--- tracker/TRKQuadrupole.hh
+++ tracker/TRKQuadrupole.hh
@@ -1,12 +1,12 @@
 #ifndef TRKQuadrupole_h
 #define TRKQuadrupole_h
 
-#include "TRKElement.h"
+#include "TRKElement.hh"
 
 class TRKQuadrupole : public TRKElement {
 public : 
   TRKQuadrupole();  
-  TRKQuadrupole(double length, double b)
+  TRKQuadrupole(double length, double b);
   ~TRKQuadrupole();
   
 private: 
