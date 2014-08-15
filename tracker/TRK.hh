5ccd8ec65b121359f1153f6c3714be3ce9c0b91a
Jochem Snuverink <Jochem.Snuverink@rhul.ac.uk>
2014-08-15 17:40:42 +0100

    add thick lens quad
    Former-commit-id: 680548681b49534c671ae3119de099c41cccbaf5

--- tracker/TRK.hh
+++ tracker/TRK.hh
@@ -1,10 +1,30 @@
 #ifndef TRK_h
 #define TRK_h
 
+#include <cmath>
+
 namespace TRK {
   const int DEFAULT_TRACKING_STEPS=10;
   const double SOL=2.9e8;
   const double e = 1.6e-19;
+
+  /** 
+   * fast way to calculate both sinh and cosh
+   * syntax similar to sincos(phi,s,c),
+   * except sincosh expects doubles and no pointers!
+   */
+  static inline void sincosh(const double a, double &s, double &c) {
+    
+    if (std::abs(a) <= 0.05) {
+      s = sinh(a);
+      c = sqrt(1.0 + s*s);
+    } else {
+      double ea = exp(a);
+      double inv_ea = 1./(ea);
+      s = (ea - inv_ea)*0.5;
+      c = (ea + inv_ea)*0.5;
+    }
+  }
 }
 
 #endif
