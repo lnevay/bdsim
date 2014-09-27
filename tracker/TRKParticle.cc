b3adef5eff1c9ee6b007ee09eb598ef465d0dd0d
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2014-09-27 13:47:15 +0100

    particle class - uses two sets of coordinates for before and after step
    Former-commit-id: e78d57434db96c8254dd5f062df304702f937bd3

--- tracker/TRKParticle.cc
+++ tracker/TRKParticle.cc
@@ -1,12 +1,46 @@
 #include "TRKParticle.hh"
+#include "vector6.hh"
 
-TRKParticle::TRKParticle()
-{}
+TRKParticle::TRKParticle():beforeindex(0)
+{
+  posmom[2] = {vector6(),vector6()};
+}
 
-TRKParticle::TRKParticle(vector6 v6):vector6(v6)
-{}
+TRKParticle::TRKParticle(vector6 coordsIn, double energyIn, double massIn, int chargeIn):beforeindex(0)
+{
+  posmom[0] = coordsIn;
+  posmom[1] = vector6();
+  energy    = energyIn;
+  mass      = massIn;
+  charge    = chargeIn;
+}
+
+TRKParticle::TRKParticle(double vIn[]):beforeindex(0)
+{
+  //x,y,s,xp,yp,sp,energy,mass,charge
+  posmom[0] = vector6(vIn[0],vIn[1],vIn[2],vIn[3],vIn[4],vIn[5]);
+  posmom[1] = vector6();
+  energy    = vIn[6]
+  mass      = vIn[7];
+  charge    = (int)vIn[8];
+}
 
 TRKParticle::~TRKParticle() {}
+
+vector6 GetBefore()
+{
+  return posmom[beforeindex];
+}
+
+vector6 GetAfter()
+{
+  return posmom[(int)!(bool)beforeindex];
+}
+
+void ConfirmNewCoordinates()
+{
+  beforeindex = (int)!(bool)beforeindex;
+}
 
 
 std::ostream& operator<< (std::ostream &out, const TRKParticle &part)
--- tracker/TRKParticle.hh
+++ tracker/TRKParticle.hh
@@ -7,32 +7,32 @@
 class TRKParticle : public vector6 { 
 public: 
   TRKParticle();
-  TRKParticle(vector6);
+  TRKParticle(vector6 coordsIn, double energyIn, double massIn, int chargeIn);
+  TRKParticle(double []);
   ~TRKParticle();
+  
+  //accessors
+  vector6 GetBefore();
+  vector6 GetAfter();
+
+  //toggle the beforeindex
+  void ConfirmNewCoordinates();
 
   /// output stream
-  //  friend std::ostream& operator<< (std::ostream &out, const TRKParticle &part);
+  friend std::ostream& operator<< (std::ostream &out, const TRKParticle &part);
 
 private:
-  // vector3 pos;
-  // vector3 mom;
-  // double mass;
-  // double charge;   
-
-public:
-  // double X0(){return pos.X();
-  // double Y0(){return pos.Y();
-  // double Z0(){return pos.Z();
-  // double Xp(){return mom.X();
-  // double Yp(){return mom.Y();
-  // double Zp(){return mom.Z();
-
-  // double x0;
-  // double y0;
-  // double z0;
-  // double xp;
-  // double yp;
-  // double zp;
+  //position and momentum have two values - before and after
+  vector6 posmom [2];
+  //energy,mass and charge don't change in the tracker!
+  double  energy;
+  double  mass;
+  double  charge;
+  
+  //need the index of posmom to look for 
+  //flip each tracking step to avoid copying
+  //only modify index which getters use
+  int beforeindex; 
 };
 
 #endif
