34b677f04248edb8d71bd37abdd763b06f32d8bb
Jochem Snuverink <Jochem.Snuverink@rhul.ac.uk>
2014-09-29 16:43:48 +0100

    cleanup of particledefinition; fix muon charge
    Former-commit-id: fdd626f32f79f8cb0ee59031508c471354b81571

--- tracker/TRKParticleDefinition.hh
+++ tracker/TRKParticleDefinition.hh
@@ -1,24 +1,26 @@
 #ifndef TRKPARTICLEDEFINITION_h
 #define TRKPARTICLEDEFINITION_h 1
 
-#include "CLHEP/Units/SystemOfUnits.h"
-#include "CLHEP/Units/PhysicalConstants.h"
 #include <map>
-#include <utility>
 #include <string>
+
+/**
+ * @brief class that holds constants related to elementary particles
+ */
 
 class TRKParticleDefinition {
 public:
   ~TRKParticleDefinition();
-  static TRKParticleDefinition* Instance();   ///access method for instance
+  static TRKParticleDefinition* Instance();   ///> access method for instance
   std::pair<double,int> GetParticleMassAndCharge(const std::string particlename);
 
 protected:
   TRKParticleDefinition();
 private:
-  static TRKParticleDefinition* _instance; ///singleton patter
+  static TRKParticleDefinition* _instance; ///> singleton pattern
+  /// map of particle name and mass in MeV and elementary charge
   std::map<std::string,std::pair<double, int> > particleMassCharge;
-  std::map<std::string,std::pair<double, int> > PrepareDefinitions();
+  void PrepareDefinitions();
 };
 
 #endif
