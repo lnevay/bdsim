9dcc3de5fbefe08ffd9a3f223a9d9e0c0717d5cd
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2014-09-29 17:55:45 +0100

    added simple 1 particle output tests
    Former-commit-id: 2269adc8ca31f9dc546a634db0ca9a2f48000a08

--- test/TRKBunchTest.cc
+++ test/TRKBunchTest.cc
@@ -7,6 +7,9 @@
 #include "tracker/TRKStrategy.hh"
 #include "tracker/TRKTracker.hh"
 #include "tracker/TRKHybrid.hh"
+#include "tracker/TRKThin.hh"
+#include "tracker/TRKAperture.hh"
+#include "tracker/TRKQuadrupole.hh"
 
 #include "BDSExecOptions.hh"
 
@@ -26,7 +29,26 @@ int main (int argc, char** argv){
   TRKBunch* bunch   = new TRKBunch(options);
 
   std::cout << *bunch << std::endl;
-  
+
+  //create strategy / set of routines
+  TRKStrategy* strategy = new TRKThin(1/*ntrackingsteps*/);
+
+  //create a single element
+  TRKAperture* ap = new TRKAperture(5.0,5.0,TRKAperture::rectangle);
+  TRKPlacement* pl = new TRKPlacement();
+  TRKQuadrupole* quad = new TRKQuadrupole(0.1,"quad1",2.5,ap,pl);  
+
+  strategy->Track(quad,bunch);
+
+  std::cout << *bunch << std::endl;
+
+  TRKParticle& part = *(bunch->begin());
+  part.SetPos(vector3(1,2,3));
+  std::cout << part << std::endl;
+  part.SetMom(vector3(0.001,0.001,1.0));
+  std::cout << part << std::endl;
+  part.ConfirmNewCoordinates();
+  std::cout << part << std::endl;
   
   return 0;
 }
