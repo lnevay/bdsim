45168aec0121ab767235ce0bb30a9b3515e8d1a7
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2014-10-13 16:54:17 +0100

    root output now compiles - testing still on going
    Former-commit-id: 7f1bd2cb9495bfd0a5baea2ad6598b68a1107a03

--- tracker/TRKOutput.cc
+++ tracker/TRKOutput.cc
@@ -10,7 +10,6 @@ BDSOutputBase* TRK::InitialiseOutput() {
   if (BDSExecOptions::Instance()->GetOutputFormat() == BDSOutputFormat::_ASCII) {
     return new BDSOutputASCII();
 }
-/*
   else if (BDSExecOptions::Instance()->GetOutputFormat() == BDSOutputFormat::_ROOT) {
 #ifdef USE_ROOT
     return new BDSOutputROOT();
@@ -18,9 +17,6 @@ BDSOutputBase* TRK::InitialiseOutput() {
     return new BDSOutputASCII();
 #endif
   }
-//If I include this section of code, I get a linker error of duplicate symbol _main...
-//don't need root output just now.
-*/
   else {
     return new BDSOutputASCII();
   }
