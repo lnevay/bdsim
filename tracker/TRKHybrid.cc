9362a795bcf53aa18b4c8397f8cdc54664499e5d
Jaime van Oers <jaimevanoers@gmail.com>
2015-03-04 17:59:39 +0000

    Removed sextupole routine for reconsideration later
    Former-commit-id: 3b967478cc44b9b1cdc1291ee6ab41859ee44b3c

--- tracker/TRKHybrid.cc
+++ tracker/TRKHybrid.cc
@@ -455,82 +455,8 @@ void TRKHybrid::Track(TRKSextupole* el, TRKBunch* bunch) {
 #ifdef TRKDEBUG
   std::cout << __METHOD_NAME__ << " Sextupole" << std::endl;
 #endif
-  double strength = el->GetStrength();
-  // note from TRKSextupole.hh strength is in T/m^2
-  // transverse coords are in um
-  // need to change routine below for units to match
-  if (std::abs(strength)<=1e-12) {
-    return Track((TRKDrift*)el,bunch);
-  }
-
-  const double h = el->GetLength()*1e6/trackingSteps; //convert from m to um
-
-  TRKBunchIter iter = bunch->begin();
-  TRKBunchIter end = bunch->end();
-
-  for (;iter!=end;++iter) {
-    TRKParticle& part = *iter;
-    for (int i=0; i<trackingSteps; i++) {
-      double x0 = part.X();
-      double y0 = part.Y();
-      double z0 = part.Z();
-      double xp = part.Xp();
-      double yp = part.Yp();
-      double zp = part.Zp();
-
-      // from BDSSextStepper
-      // Evaluate field at the approximate midpoint of the step.
-      x0=x0+xp*h/2;
-      y0=y0+yp*h/2;
-
-      double x02My02=(x0*x0-y0*y0);
-
-      // local r'' (for curvature)
-      vector3 LocalRpp(-zp*x02My02, 2*zp*x0*y0, xp*x02My02-2*yp*x0*y0);
-
-      //G4cout << "LocalRpp: " <<LocalRpp<< G4endl;
-
-      LocalRpp*=strength/2; // 2 is actually a 2! factor.
-      // determine effective curvature
-      double R_1 = LocalRpp.mag();
-
-      if (R_1==0) {
-	// TODO
-	//return drift->Track(vIn,vOut,h);
-      }
-
-      double h2=h*h;
-      // chord distance (simple quadratic approx)
-      //itsDist= h2*R_1/8;
-
-      // Save for Synchrotron Radiation calculations:
-      //BDSLocalRadiusOfCurvature=1./R_1;
-
-      double dx=xp*h + LocalRpp.X()*h2 /2.;
-      double dy=yp*h + LocalRpp.Y()*h2 /2.;
-
-      double dz=sqrt(h2*(1.-h2*R_1*R_1/12)-dx*dx-dy*dy);
-      // check for precision problems
-      double ScaleFac=(dx*dx+dy*dy+dz*dz)/h2;
-      if(ScaleFac>1.0000001)
-	{
-	  ScaleFac=sqrt(ScaleFac);
-	  dx/=ScaleFac;
-	  dy/=ScaleFac;
-	  dz/=ScaleFac;
-	}
-
-      double vOut[6];
-      vOut[0] = x0+dx;
-      vOut[1] = y0+dy;
-      vOut[2] = z0+dz;
-
-      vOut[3] += h*LocalRpp.X();
-      vOut[4] += h*LocalRpp.Y();
-      vOut[5] += h*LocalRpp.Z();
-      part.SetPosMom(vector6(vOut));
-    }
-  }
+  // TODO Implement sextupole tracking
+  Track((TRKDrift*)el,bunch);
 }
 
 void TRKHybrid::Track(TRKOctupole* el, TRKBunch* bunch) {
