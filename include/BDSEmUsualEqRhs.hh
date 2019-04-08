526377cf6c0d82f7d22d7c7d84ced77675eff23c
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-04-08 17:24:45 +0200

    Fix compiler warning.
    Former-commit-id: 0055bbc2a92d482ef79fd63216572e4a1fa6b318

--- include/BDSEmUsualEqRhs.hh
+++ include/BDSEmUsualEqRhs.hh
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifndef BDSEMSUALEQRHS_H
+#ifndef BDSEMUSUALEQRHS_H
 #define BDSEMUSUALEQRHS_H
 
 #include "BDSAuxiliaryNavigator.hh"
