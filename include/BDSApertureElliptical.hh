e5caa3cbc837f17339663a9a586e88c7cac6156e
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-12-31 14:44:15 +0000

    Elliptical aperture class.
    Former-commit-id: ea73143e461f477d4f7f98e92d13d6f9c1e2a2ef

---
+++ include/BDSApertureElliptical.hh
@@ -0,0 +1,54 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSAPERTUREELLIPTICAL_H
+#define BDSAPERTUREELLIPTICAL_H
+
+#include "BDSAperture.hh"
+
+#include "globals.hh"
+
+/**
+ * @brief Elliptical aperture.
+ *
+ * @author Laurie Nevay
+ */
+
+class BDSApertureElliptical: public BDSAperture
+{
+public:
+  BDSApertureElliptical(G4double aIn,
+			G4double bIn);
+  virtual ~BDSApertureElliptical(){;}
+
+  G4double a;
+  G4double b;
+
+  virtual G4bool    Equals(const BDSAperture* other)   const;
+  virtual G4bool    LessThan(const BDSAperture* other) const;
+  virtual void      CheckInfoOK()                      const;
+  virtual G4double  RadiusToEncompass()                const;
+  virtual BDSExtent Extent()                           const;
+
+  BDSApertureElliptical        operator+ (const G4double number) const;
+  const BDSApertureElliptical& operator+=(const G4double number);
+  BDSApertureElliptical        operator* (const G4double number) const;
+  const BDSApertureElliptical& operator*=(const G4double number);
+};
+
+#endif
---
+++ src/BDSApertureElliptical.cc
@@ -0,0 +1,115 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSAperture.hh"
+#include "BDSApertureCircular.hh"
+#include "BDSApertureElliptical.hh"
+#include "BDSApertureType.hh"
+#include "BDSExtent.hh"
+
+#include "G4Types.hh"
+
+BDSApertureElliptical::BDSApertureElliptical(G4double aIn,
+					     G4double bIn):
+  BDSAperture(BDSApertureType::elliptical),
+  a(aIn),
+  b(bIn)
+{;}
+
+G4bool BDSApertureElliptical::Equals(const BDSAperture* other) const
+{
+  if (!other)
+    {return false;}
+  else if (other->apertureType != apertureType)
+    {return false;}
+  else
+    {
+      const BDSApertureElliptical* oc = dynamic_cast<const BDSApertureElliptical*>(other);
+      return oc->a == a && oc->b == b;
+    }
+}
+
+G4bool BDSApertureElliptical::LessThan(const BDSAperture* other) const
+{
+  if (!other)
+    {return false;}
+
+  G4bool result = false;
+  BDSApertureType otherType = other->apertureType;
+  switch (otherType.underlying())
+    {
+    case BDSApertureType::elliptical:
+      {
+	const BDSApertureElliptical* oc = dynamic_cast<const BDSApertureElliptical*>(other);
+	result = a < oc->a && b < oc->b;
+	break;
+      }
+    case BDSApertureType::circular:
+      {
+	const BDSApertureCircular* oc = dynamic_cast<const BDSApertureCircular*>(other);
+	result = a < oc->radius && b < oc->radius;
+	break;
+      }
+    default:
+      {break;}
+    }
+  return result;
+}
+
+void BDSApertureElliptical::CheckInfoOK() const
+{
+  CheckRequiredParametersSet(a, true, b, true);
+}
+
+BDSExtent BDSApertureElliptical::Extent() const
+{
+  BDSExtent simpleExtent(a, b, 0);
+  return ExtentOffsetTilt(simpleExtent);
+}
+
+BDSApertureElliptical BDSApertureElliptical::operator+ (const G4double number) const
+{
+  BDSApertureElliptical result = BDSApertureElliptical(*this);
+  result.a += number;
+  result.b += number;
+  return result;
+}
+
+const BDSApertureElliptical& BDSApertureElliptical::operator+=(const G4double number)
+{
+  a += number;
+  b += number;
+  return *this;
+}
+
+BDSApertureElliptical BDSApertureElliptical::operator* (const G4double number) const
+{
+  BDSApertureElliptical result = BDSApertureElliptical(*this);
+  result.a *= number;
+  result.b *= number;
+  return result;
+}
+
+const BDSApertureElliptical& BDSApertureElliptical::operator*=(const G4double number)
+{
+  a *= number;
+  b *= number;
+  return *this;
+}
+
+
