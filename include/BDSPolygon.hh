3692978151a4b69cbb3d1993127d4176f02a0f49
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2020-01-02 13:45:10 +0000

    Introduce a polygon class that'll handle simple and complex 2D polygons.
    Former-commit-id: 334b5e0a3b7b3b6aeb8a2a6a590a13a1ac75c08e

---
+++ include/BDSPolygon.hh
@@ -0,0 +1,86 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSPOLYGON_H
+#define BDSPOLYGON_H
+
+#include "G4TwoVector.hh"
+#include "G4Types.hh"
+
+#include <iterator>
+#include <vector>
+
+class BDSTiltOffset;
+
+/**
+ * @brief A 2D polygon made of line segments.
+ *
+ * @author Laurie Nevay
+ */
+
+class BDSPolygon
+{
+public:
+  BDSPolygon(const std::vector<G4TwoVector>& pointsIn);
+  ~BDSPolygon(){;}
+
+  /// Accessor.
+  const std::vector<G4TwoVector>& Points() const {return points;}
+  
+  /// Return true if point inside this polygon.
+  G4bool Inside(const G4TwoVector& point) const;
+
+  /// Return true if polygon is completely contained inside this one.
+  /// If any lines intersect, this will return false.
+  G4bool Inside(const BDSPolygon& other) const;
+
+  /// Return true if any segments that make this polyon intersect with
+  /// any others.
+  G4bool SelfIntersecting(G4int* indexOfIntersectionA = nullptr,
+			  G4int* indexOfIntersectionB = nullptr) const;
+  
+  /// Return a copy with a tilt and offset applied.
+  BDSPolygon ApplyTiltOffset(const BDSTiltOffset& to) const;
+
+  ///@{ Iterator mechanics.
+  typedef std::vector<G4TwoVector>::iterator               iterator;
+  typedef std::vector<G4TwoVector>::const_iterator         const_iterator;
+  typedef std::vector<G4TwoVector>::reverse_iterator       reverse_iterator;
+  typedef std::vector<G4TwoVector>::const_reverse_iterator const_reverse_iterator;
+  iterator               begin()        {return points.begin();}
+  iterator               end()          {return points.end();}
+  const_iterator         begin()  const {return points.begin();}
+  const_iterator         end()    const {return points.end();}
+  reverse_iterator       rbegin()       {return points.rbegin();}
+  reverse_iterator       rend()         {return points.rend();}
+  const_reverse_iterator rbegin() const {return points.rbegin();}
+  const_reverse_iterator rend()   const {return points.rend();}
+  G4bool                 empty()  const {return points.empty();}
+  ///@}
+
+  inline G4TwoVector front() const {return points.front();}
+  inline G4TwoVector back()  const {return points.back();}
+  std::vector<G4TwoVector>::size_type size() const {return points.size();}
+
+private:
+  BDSPolygon() = delete;
+  
+  std::vector<G4TwoVector> points;
+};
+
+#endif
---
+++ src/BDSPolygon.cc
@@ -0,0 +1,76 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSDebug.hh"
+#include "BDSException.hh"
+#include "BDSPolygon.hh"
+#include "BDSTiltOffset.hh"
+
+#include "G4TwoVector.hh"
+#include "G4Types.hh"
+
+#include <vector>
+
+BDSPolygon::BDSPolygon(const std::vector<G4TwoVector>& pointsIn):
+  points(pointsIn)
+{
+  if (points.size() < 3)
+    {throw BDSException(__METHOD_NAME__, "polygon must have at least 3 x,y points to be valid.");}
+}
+
+G4bool BDSPolygon::Inside(const G4TwoVector& point) const
+{
+  G4bool result = true;
+  for (G4int i = 0; i < (G4int)size() - 1; i++)
+    {
+      G4TwoVector test = points[i] - point;
+      G4TwoVector norm = (points[i+1] - points[i]).orthogonal();
+      result &= test.dot(norm) > 0;
+    }
+
+  return result;
+}
+
+G4bool BDSPolygon::Inside(const BDSPolygon& other) const
+{
+  G4bool result = true;
+  for (G4int i = 0; i < (G4int)size() - 1; i++)
+    {
+
+
+    }
+  return result;
+}
+
+G4bool BDSPolygon::SelfIntersecting(G4int* indexOfIntersectionA,
+				    G4int* indexOfIntersectionB) const
+{return false;}
+
+BDSPolygon BDSPolygon::ApplyTiltOffset(const BDSTiltOffset& to) const
+{
+  BDSPolygon r(*this);
+  
+  G4double    t      = to.Tilt();
+  G4TwoVector offset = to.GetOffset2D();
+  for (auto& p : r)
+    {
+      p.rotate(t);
+      p += offset;
+    }
+  return r;
+}
