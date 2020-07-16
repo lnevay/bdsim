8988cba78eee8b1a914081aff8358ce2eafd1d16
swalker92 <zwap082@live.rhul.ac.uk>
2020-07-16 15:16:21 +0100

    Non-linear drift map
    Former-commit-id: 4b92e802840e4974462f09a1d1f93eb5ebaee284

--- tracker/TRKMaps.hh
+++ tracker/TRKMaps.hh
@@ -7,7 +7,9 @@ namespace trk {
 namespace maps {
 
 void drift(TRKParticle &, double length) noexcept;
+void nldrift(TRKParticle &, double length) noexcept;
 void drift(TRKBunch &, double length) noexcept;
+void nldrift(TRKBunch &, double length) noexcept;
 void sbend(TRKBunch &, double length, double angle, double k1 = 0.0) noexcept;
 void dipole_fringe(TRKParticle &, double k0, double angle) noexcept;
 void dipole_fringe(TRKBunch &, double k0, double angle) noexcept;
