2c723db1d99ba0f3766869c6c615b8a4314526d2
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2020-07-28 12:18:46 +0100

    Implement the dipole fringe as a separate beamline element.
    Former-commit-id: eb772e6836f45cfc22bc4fff5507881dda113987

---
+++ tracker/TRKDipoleFringe.cc
@@ -0,0 +1,44 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "TRKDipoleFringe.hh"
+#include "TRKStrategy.hh"
+
+TRKDipoleFringe::TRKDipoleFringe(
+            std::string   nameIn,
+		   double        polefaceIn,
+		   TRKAperture  *apertureIn,
+		   TRKPlacement *placementIn,
+		   double        k0In=0.0):
+  TRKElement(nameIn, 0.0, apertureIn, placementIn),
+  poleface(polefaceIn), k0(k0In)
+{;}
+
+TRKDipoleFringe::~TRKDipoleFringe()
+{;}
+
+void TRKDipoleFringe::Track(TRKParticle* particle, double step, TRKStrategy* strategy)
+{
+  strategy->Track(this, particle, step);
+}
+
+void TRKDipoleFringe::Print(std::ostream &out) const
+{
+  TRKElement::Print(out);
+  out << "; poleface: " << poleface << "rad" << ", k0: " << k0;
+}
---
+++ tracker/TRKDipoleFringe.hh
@@ -0,0 +1,58 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef TRKDipoleFringe_h
+#define TRKDipoleFringe_h
+
+#include "TRKElement.hh"
+
+/**
+ * @brief dipole fringe
+ * 
+ * dipole fringe tracking
+ */
+
+class TRKDipoleFringe: public TRKElement
+{
+public:
+    TRKDipoleFringe(std::string   name,
+	   double        poleface,
+	   TRKAperture  *aperture,
+	   TRKPlacement *placement,
+	   double        k0);
+
+  virtual ~TRKDipoleFringe();
+  
+  inline double GetPoleface() const {return poleface;}
+  inline double GetK0() const {return k0;}
+  
+  virtual void Track(TRKParticle* particle, double step, TRKStrategy* strategy);
+  
+protected:
+  /// output stream
+  virtual void Print(std::ostream& out) const;
+
+private:
+  TRKDipoleFringe(); ///< not implemented
+
+  /// Poleface in radians
+  double poleface;
+  double k0;
+};
+
+#endif
