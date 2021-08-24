bd4b4ec95095664eb90ded0daa02d47d969d6f3f
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-08-24 11:21:04 +0200

    Includes.
    Former-commit-id: ebd968d263188f9480052115ed77cb35ebc873a0

--- src/BDSMagnetNonSplitOuter.cc
+++ src/BDSMagnetNonSplitOuter.cc
@@ -49,6 +49,9 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #include "parser/element.h"
 #include "parser/elementtype.h"
 
+#include <algorithm>
+#include <cmath>
+
 using namespace GMAD;
 
 class G4Userlimits;
