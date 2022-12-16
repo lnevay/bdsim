acc35f8a5b04408a602b62e4ba0825fe251bfb8d
Stefan Alexandru Ghinescu <stefan.alexandru.ghinescu@cern.ch>
2022-12-16 18:05:16 +0200

    Added UserTrackInfo class for muon biasing
    Former-commit-id: aa8ca71debd240ff7db330df5f2e82c6bcb023f3

---
+++ include/BDSMuonFluxEnhancementTrackInformation.hh
@@ -0,0 +1,64 @@
+#ifndef BDSMuonFluxEnhancementTrackInformation_hh
+#define BDSMuonFluxEnhancementTrackInformation_hh 1
+
+#include "G4Track.hh"
+#include "G4VUserTrackInformation.hh"
+
+enum MuonFluxTrackType{
+  kOriginalTrack,
+  kCloneTrack
+};
+
+class BDSMuonFluxEnhancementTrackInformation : public G4VUserTrackInformation
+{
+public:
+  BDSMuonFluxEnhancementTrackInformation(): G4VUserTrackInformation("muonFluxEnhancement"),
+    fIsAllowedInelastic(true),fParentPDGid(0), fTrackType(kOriginalTrack), fHasClone(false){}
+
+  BDSMuonFluxEnhancementTrackInformation(const BDSMuonFluxEnhancementTrackInformation &info): G4VUserTrackInformation("muonFluxEnhancement"),
+    fIsAllowedInelastic(info.fIsAllowedInelastic), fParentPDGid(info.fParentPDGid), fTrackType(info.fTrackType), fHasClone(info.fHasClone){}
+
+  ~BDSMuonFluxEnhancementTrackInformation() {};
+
+  inline void *operator new(size_t);
+  inline void operator delete(void *info);
+  
+  void Print() const;
+
+public:
+  G4bool GetIsAllowedInelastic() const {return fIsAllowedInelastic;}
+  void SetIsAllowedInelastic(G4bool val) {fIsAllowedInelastic = val;}
+
+  G4int GetParentPDGid() const {return fParentPDGid;}
+  void SetParentPDGid(G4int val) {fParentPDGid = val;}
+  
+  MuonFluxTrackType GetTrackType() const {return fTrackType;}
+  void SetTrackType(MuonFluxTrackType val) {fTrackType = val;}
+
+  G4bool GetHasClone() const {return fHasClone;}
+  void SetHasClone(G4bool val) {fHasClone = val;}
+
+private:
+  G4bool fIsAllowedInelastic;
+  G4int fParentPDGid;
+  MuonFluxTrackType fTrackType;
+  G4bool fHasClone;
+
+};
+
+extern G4ThreadLocal
+ G4Allocator<BDSMuonFluxEnhancementTrackInformation> * aTrackInformationAllocator;
+
+inline void* BDSMuonFluxEnhancementTrackInformation::operator new(size_t)
+{
+  if (!aTrackInformationAllocator)
+    aTrackInformationAllocator = new G4Allocator<BDSMuonFluxEnhancementTrackInformation>;
+  return (void*)aTrackInformationAllocator->MallocSingle();
+}
+
+inline void BDSMuonFluxEnhancementTrackInformation::operator delete(void *info){
+  aTrackInformationAllocator->FreeSingle((BDSMuonFluxEnhancementTrackInformation*) info);
+}
+
+
+#endif
