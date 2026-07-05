437ae8dce788af680dc777cd5335c4fc232910bb
Laurie Nevay <laurie.nevay@cern.ch>
2026-07-05 19:20:30 +0200

    Tidying.

--- src/BDSAperture.cc
+++ src/BDSAperture.cc
@@ -28,6 +28,9 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #include "G4String.hh"
 #include "G4Types.hh"
 
+#include <cmath>
+#include <vector>
+
 BDSAperture::BDSAperture(BDSApertureType apertureTypeIn):
   apertureType(apertureTypeIn)
 {;}
@@ -69,7 +72,7 @@ void BDSAperture::CheckRequiredParametersSet(G4double aper1, G4bool aper1Set,
 }
 
 void BDSAperture::CheckParameterIsPositive(G4double        parameter,
-					   const G4String& nameToWarn) const
+                                           const G4String& nameToWarn) const
 {
   if (parameter <= 0)
     {throw BDSException(__METHOD_NAME__, "parameter \"" + nameToWarn + "\" is not >= 0");}
