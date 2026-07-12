2a7a2eff8e5613f8f5f13f2f9caa6645d5ab0f98
Laurie Nevay <laurie.nevay@cern.ch>
2026-07-12 12:06:57 +0200

    Previous fix was wrong for racetrack.

--- src/BDSApertureRaceTrack.cc
+++ src/BDSApertureRaceTrack.cc
@@ -20,8 +20,6 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #include "BDSApertureCurved.hh"
 #include "BDSApertureRaceTrack.hh"
 #include "BDSApertureType.hh"
-#include "BDSDebug.hh"
-#include "BDSException.hh"
 #include "BDSExtent.hh"
 #include "BDSPolygon.hh"
 #include "BDSTiltOffset.hh"
@@ -68,8 +66,6 @@ void BDSApertureRaceTrack::CheckInfoOK() const
   CheckParameterIsPositive(x, "x");
   CheckParameterIsPositive(y, "y");
   CheckParameterIsPositive(radius, "radius");
-  if (radius > x || radius > y)
-    {throw BDSException(__METHOD_NAME__, "radius must be less than both x and y");}
 }
 
 BDSExtent BDSApertureRaceTrack::Extent() const
