11df53d87ba32f1551ec16fa1ae55383b92f4448
Laurie Nevay <laurie.nevay@cern.ch>
2026-09-11 20:29:43 +0200

    Do not double delete the user limits potentially.

--- src/BDSFieldInfo.cc
+++ src/BDSFieldInfo.cc
@@ -145,7 +145,8 @@ BDSFieldInfo::~BDSFieldInfo()
 {
   delete magnetStrength;
   delete transform;
-  delete stepLimit;
+  if (stepLimit != defaultUL)
+    {delete stepLimit;}
   delete transformBeamline;
   delete extraInfo;
 }
